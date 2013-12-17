c5dd45e044a56b142a600857dd2cba9329c13374
App Academy Student <student@appacademy.io>
2013-12-17 15:33:55 -0800

    SQL object

--- lib/active_record_lite/02_sql_object.rb
+++ lib/active_record_lite/02_sql_object.rb
@@ -28,41 +28,54 @@ class SQLObject < MassObject
   end
 
   def self.find(id)
-    DBConnection.execute(<<-SQL, id)
+    results = DBConnection.execute(<<-SQL, id)
       SELECT
-        *
+        #{table_name}.*
       FROM
         #{table_name}
       WHERE
         #{table_name}.id = ?
     SQL
-    parse_all(results)
+    parse_all(results).first
+  end
+
+
+  def insert
+    col_names = self.class.attributes.join(", ")
+    question_marks = (["?"] * (self.class.attributes.count)).join(", ")
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
+  end
+
+  def save
+    if self.id.nil?
+      self.insert
+    else
+      self.update
+    end
+  end
+
+  def update
+    set_line = self.class.attributes
+      .map { |attr| "#{attr} = ?"}.join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values, id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_line}
+      WHERE
+        #{self.class.table_name}.id = ?
+    SQL
   end
 
   def attribute_values
     self.class.attributes.map { |attr| self.send(attr)}
-  end
-
-  def insert
-    col_names = self.class.attributes.join(", ")
-    question_marks = (["?"] * (self.class.attributes.length)).join(", ")
-    DBConnection.execute(<<-SQL, *attribute_values)
-      INSERT INTO
-        #{self.class.table_name} #{col_names}
-      VALUES
-        (#{question_marks})
-    SQL
-  end
-
-  def save
-    # ...
-  end
-
-  def update
-    # ...
-  end
-
-  def attribute_values
-    # ...
   end
 end
