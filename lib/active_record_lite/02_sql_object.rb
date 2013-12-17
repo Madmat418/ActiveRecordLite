04cd58058e2265e654d269666a37303e39626e4f
App Academy Student <student@appacademy.io>
2013-12-17 14:20:53 -0800

    first commit

---
+++ lib/active_record_lite/02_sql_object.rb
@@ -0,0 +1,43 @@
+require_relative 'db_connection'
+require_relative '01_mass_object'
+require 'active_support/inflector'
+
+class MassObject
+  def self.parse_all(results)
+    # ...
+  end
+end
+
+class SQLObject < MassObject
+  def self.table_name=(table_name)
+    # ...
+  end
+
+  def self.table_name
+    # ...
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def save
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def attribute_values
+    # ...
+  end
+end
