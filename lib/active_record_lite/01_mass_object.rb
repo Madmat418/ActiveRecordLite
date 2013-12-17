2bbe012e474c4492211bfce542feaf207d49d85e
App Academy Student <student@appacademy.io>
2013-12-17 15:08:16 -0800

    Attr_accessor and MassObject

--- lib/active_record_lite/01_mass_object.rb
+++ lib/active_record_lite/01_mass_object.rb
@@ -2,14 +2,23 @@ require_relative '00_attr_accessor_object.rb'
 
 class MassObject < AttrAccessorObject
   def self.my_attr_accessible(*new_attributes)
-    # ...
+    new_attributes = new_attributes.map(&:to_sym)
+    self.attributes.concat(new_attributes)
   end
 
   def self.attributes
-    # ...
+    raise 'must not call #attributes on MassObject directly' if self == MassObject
+    @attributes ||= []
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      attr_name = attr_name.to_sym
+      if self.class.attributes.include?(attr_name)
+        self.send("#{attr_name}=", value)
+      else
+        raise "mass assignment to unregistered attribute '#{attr_name}'"
+      end
+    end
   end
 end
--- lib/active_record_lite/02_sql_object.rb
+++ lib/active_record_lite/02_sql_object.rb
@@ -4,29 +4,54 @@ require 'active_support/inflector'
 
 class MassObject
   def self.parse_all(results)
-    # ...
+    results.map {|result| self.new(result)}
   end
 end
 
 class SQLObject < MassObject
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.name.underscore.pluralize
   end
 
   def self.all
-    # ...
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+    SQL
+    parse_all(results)
   end
 
   def self.find(id)
-    # ...
+    DBConnection.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{table_name}.id = ?
+    SQL
+    parse_all(results)
+  end
+
+  def attribute_values
+    self.class.attributes.map { |attr| self.send(attr)}
   end
 
   def insert
-    # ...
+    col_names = self.class.attributes.join(", ")
+    question_marks = (["?"] * (self.class.attributes.length)).join(", ")
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} #{col_names}
+      VALUES
+        (#{question_marks})
+    SQL
   end
 
   def save
